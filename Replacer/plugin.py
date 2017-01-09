56c76e4edb9620db53e07814cd3433c0a2ade1c5
nyuszika7h <nyuszika7h@openmailbox.org>
2017-01-09 14:03:05 +0100

    Replacer: Do not allow space as delimiter
    Using spaces as delimiter is uncommon
    and it may catch legitimate conversation as well.

--- Replacer/plugin.py
+++ Replacer/plugin.py
@@ -46,7 +46,7 @@ except ImportError:
     _ = lambda x: x
 
 
-SED_PATTERN = (r"s(?P<delim>[^A-Za-z0-9\\])(?P<pattern>.*?)(?P=delim)"
+SED_PATTERN = (r"s(?P<delim>[^A-Za-z0-9\s\\])(?P<pattern>.*?)(?P=delim)"
                r"(?P<replacement>.*?)(?:(?P=delim)(?P<flags>[gi]*))?$")
 ACT_PATTERN = r"^(?i)(?:(?P<nick>[a-z_\-\[\]\\^{}|`][a-z0-9_\-\[\]\\^{}|`]*)[|:, ]{1,2})?"
 SED_REGEX = re.compile(r"^" + SED_PATTERN)
